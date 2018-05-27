ec48407f168f6bb622c71d9b4321799e8049ed74
Euryale <ambroise.eu@hotmail.fr>
2018-05-27 13:26:47 +0200

    Ajout d'un multithreading pour le chargement des données

--- I00I/I00I/Game.cpp
+++ I00I/I00I/Game.cpp
@@ -7,10 +7,17 @@ Game::Game() :
 	world(NULL),
 	gameState(LOADING)
 {
-	int a;
-	//	Création des éléments de base du jeu
+//	Création des éléments de base du jeu
 
-	//	Boucle de d'affichage de la fenetre
+	// Création du mutex pour le chargement des données
+	loadMutex = NULL;
+
+	// Création du thread de chargement
+	DWORD threadID;
+	loadThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) loading, this, 0, &threadID);
+	assert(loadThread != NULL);
+
+//	Boucle de d'affichage de la fenetre
 	while (window.isOpen()) {
 		switch (gameState) {
 		case LOADING: break;
@@ -20,6 +27,42 @@ Game::Game() :
 		}
 		window.display();
 	}
-	std::cin >> a;
+
+}
+
+void Game::displayLoading() {
+	//	Le mutex n'a pas encore été crée par le thread
+	if (loadMutex == NULL) return;
+	auto state = WaitForSingleObject(loadMutex, 10);
+	switch (state) {
+	case WAIT_ABANDONED: break;
+	case WAIT_TIMEOUT: break;
+		//	une fois que le chargement des données est terminé
+		// On relache le thread et le mutex, et l'état du programme passe en "MENU"
+	case WAIT_OBJECT_0:
+		CloseHandle(loadThread);
+		CloseHandle(loadMutex);
+		gameState = MENU;
+		break;
+	case WAIT_FAILED: break;
+	default: break;
+	}
+}
+
+DWORD Game::loading(LPVOID params) {
+	Game* that = (Game*)params;
+	// Création du mutex pour le chargement des données
+	that->loadMutex = CreateMutexA(NULL, FALSE, NULL);
+	assert(that->loadMutex != NULL);
+	auto mutexValue = WaitForSingleObject(that->loadMutex, 1);
+	if (mutexValue == WAIT_OBJECT_0) {
+		//	Chargement des données
+#ifdef DEBUG_LOG
+		std::cout << "On est en train de charger des données avec la fonction loading..." << std::endl;
+#endif
+		//	Relache du mutex
+		ReleaseMutex(that->loadMutex);
+	}
+	return 0;
 }
 
--- I00I/I00I/Game.h
+++ I00I/I00I/Game.h
@@ -18,11 +18,30 @@ enum GAME_STATE {
 class Game {
 	sf::RenderWindow window;
 	GAME_STATE gameState;
+	HANDLE loadMutex;
+	HANDLE loadThread;
 
 	//	Référence sur le monde qui sera généré dans le jeu
 	b2World* world;
-
 public :
 	Game();
+private:
+	//Méthodes de rafraichissement de l'affichage en fonction de l'état du programme
+	// Chaque méthode est amenée à etre appelée en boucle, a chaque rafraichissement de l'affichage
+	// Jusqu'à ce que la classe change d'état
 
+	//	Fonction qui affiche quelque chose pendant le chargement des données/assets en mémmoire (ce chargement est fait sur un autre thread)
+	void displayLoading();
+
+	//	Fonction qui affiche et fait réagit le menu et les paramétrages de partie
+	void displayMenu();
+
+	//	Fonction qui gère la partie
+	void displayPlaying();
+
+	//	Fonction qui s'assure qu'on ne perd aucune données avant de quitter
+	void displayUnLoading();
+
+	//	Fonction exécutée sur un autre thread et qui charge en mémoire les données du jeux, lache le sémaphore quand les données sont chargées
+	static DWORD WINAPI loading(LPVOID);
 };
