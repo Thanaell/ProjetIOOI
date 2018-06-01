1fbb78e41e10b1ae588a582ee18fa22d1e3146b3
Thanaell <gaelle_clavelin@orange.fr>
2018-06-01 14:40:54 +0200

    Prototypes des premières méthodes

--- I00I/I00I/Personnage.h
+++ I00I/I00I/Personnage.h
@@ -2,9 +2,16 @@
 #include "stdafx.h"
 #include "PlayingElement.h"
 #include "SpellType.h"
+#include "CharacterType.h"
+#include "Spell.h"
 
 class Personnage : public PlayingElement {
 	std::string name;
 	int health;
 	std::vector<SpellType> spellbook;
+public:
+	Personnage(CharacterType type);
+	Spell& throwSpell(SpellType sort);
+	void receive(SpellType sort);
+
 };
--- I00I/I00I/Spell.h
+++ I00I/I00I/Spell.h
@@ -7,4 +7,7 @@ class Spell : public PlayingElement {
 	std::string name;
 	SpellType type;
 	double range; 
+public:
+	Spell(SpellType type);
+	void affect(Personnage const & character);
 };
